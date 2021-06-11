450b2e09877a617c15cbe71a9ed9446b5f775723
Vlad Dubovski <dubikvlad@mail.ru>
2021-06-11 14:53:26 +0300

    added scroll to contacts

--- src/js/script.js
+++ src/js/script.js
@@ -57,3 +57,19 @@ function openNav() {
 function closeNav() {
     document.getElementById("nav").style.height = "0%";
 }
+
+// плавный скролл
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+for (let anchor of anchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+}
