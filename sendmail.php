c8f2e54206256911b5e902cd6aafc755c34e39a0
Vlad Dubovski <dubikvlad@mail.ru>
2021-06-27 12:43:53 +0300

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -1,14 +1,14 @@
 <?php
     use PHPMailer\PHPMailer\PHPMailer;
     use PHPMailer\PHPMailer\Exception;
-
+    
     require 'phpmailer/src/Exception.php';
     require 'phpmailer/src/PHPMailer.php';
     require 'phpmailer/src/SMTP.php';
 
     $mail = new PHPMailer(true);
     $mail->CharSet = 'UTF-8';
-    $mail->setLanguge('ru' , 'phpmailer/language/');
+    $mail->setLanguage('ru' , 'phpmailer/language/');
     $mail->IsHTML(true);
 
     $name = $_POST['name'];
@@ -18,32 +18,32 @@
     $mail->isSMTP();                                      // Set mailer to use SMTP
     $mail->Host = 'smtp.mail.ru';  						  // Specify main and backup SMTP servers
     $mail->SMTPAuth = true;                               // Enable SMTP authentication
-    $mail->Username = 'web-meert1@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
-    $mail->Password = 'UYaOaijRr23*'; // Ваш пароль от почты с которой будут отправляться письма
+    $mail->Username = 'web-meert1@mail.ru';               // Ваш логин от почты с которой будут отправляться письма
+    $mail->Password = 'UYaOaijRr23*';                     // Ваш пароль от почты с которой будут отправляться письма
     $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-    $mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
+    $mail->Port = 465;                                    // TCP port to connect to / этот порт может отличаться у других провайдеров    
     //От кого письмо
-    $mail->setFrom('web-meert1@mail.ru' , 'Вася');
+    $mail->setFrom('web-meert1@mail.ru');
     //Кому
     $mail->addAddress('dubikvlad@mail.ru');
     //Тема
-    $mail->Subject = "Новая заявка на сайте";
+
 
     //Тело письма
 
-    $body = '<h1>Новая заявка</h1>';
+    //$body = '<h1>Новая заявка</h1>';
 
-    if(trim(!empty($_POST['name']))) {
-        $body.='<p><strong>Имя: </strong> ' $name '.</p>';
-    }
-    if(trim(!empty($_POST['email']))) {
-        $body.='<p><strong>E-mail: </strong> ' $email '.</p>';
-    }
-    if(trim(!empty($_POST['phone']))) {
-        $body.='<p><strong>Телефон: </strong> ' $phone '.</p>';
-    }
+    // if(trim(!empty($_POST['name']))) {
+    //     $body ='<p><strong>Имя: </strong> ' .$name '.</p>';
+    // }
+    // if(trim(!empty($_POST['email']))) {
+    //     $body ='<p><strong>E-mail: </strong> ' .$email '.</p>';
+    // }
+    // if(trim(!empty($_POST['phone']))) {
+    //     $body ='<p><strong>Телефон: </strong> ' .$phone '.</p>';
+    // }
 
-    $mail->Body = $body;
+    $mail->Body    = '' .$name . ' оставил заявку, его телефон ' .$phone. '<br>Почта этого пользователя: ' .$email;
 
     //отправка
     if($mail->send()) {
